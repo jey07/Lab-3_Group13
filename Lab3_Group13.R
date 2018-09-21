c983b8972abddfc609fd9844587a0e31425fe506
Naveen Joseph Gabriel <navga709@student.liu.se>
2018-09-21 23:33:35 +0200

    Updated R code for Assignment 2 with answer for 4th question. Changed the rds file as add_sf function only accepts sf type file

--- Lab3_Group13.R
+++ Lab3_Group13.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+library(plotly)
+
 
 #Reading file
 swedfile<-read.csv("Swedish_Household.csv")
@@ -15,11 +17,28 @@ levels(swedfile$Region)<-sapply(columnsplit,function(x) x[[2]])
 #Spreading the data from long to wide based on factors of Age
 new_swedfile<-tidyr::spread(swedfile,Age,Mean_Income)
 #colnames(new_swedfile)<-c("Region","Yo","Mean_Income")
+new_swedfile$Region=as.character(new_swedfile$Region)
+
+#Changing the names of region in the file from csv to match region names in sf file
+new_swedfile[new_swedfile$Region=="Örebro",]$Region<-"Orebro"
+new_swedfile[new_swedfile$Region=="Västra",]$Region<-"Västra Götaland"
 
 
 #Violin Plot for three Age group showing mean income in SEK 
 p<-ggplot(swedfile,aes(Age,Mean_Income,color=Age)) + geom_violin()
-p+stat_summary(fun.y=median,geom="point",color="blue") +xlab("\n Age Group ") +
+   
+  p+stat_summary(fun.y=median,geom="point",color="blue") +xlab("\n Age Group ") +
   ylab("Mean Income in SEK\n")
+  
+#Modifying the rownames  
+rownames(new_swedfile)<-new_swedfile$Region
 
+#Adding Mean values of Young and Adult age group corresponding to the name from
+#rds file
+swedimage$Young<-new_swedfile[swedimage$NAME_1,"Young"]
+swedimage$Adult<-new_swedfile[swedimage$NAME_1,"Adult"]
 
+#Plotting chlorpeth plot of sweden counties based on mean income of Young and Adult respectively.)
+p<-plot_ly() %>% add_sf(data = swedimage,color=~Young,split=~NAME_1,colors="Reds")
+
+p2<-plot_ly() %>% add_sf(data = swedimage,color=~Adult,split=~NAME_1,colors="Blues")
